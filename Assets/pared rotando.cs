41c9dfd476047387eeb20b928fea9f2343d404d1
jordanPRdanvekin <perezramosjordan02@gmail.com>
2023-10-02 17:00:00 +0100

    dia 1 hemos rescrito el escenario
    añadimos el scrite del personaje modificado el escenario y ademas hemos añadido el efecto de rotacion en los siguientes puntos ademas de darle vida al murciegalo

--- Assets/pared rotando.cs
+++ Assets/pared rotando.cs
@@ -4,38 +4,12 @@ using UnityEngine;
 
 //ej2 pared rotacional
 public class paredrotando : MonoBehaviour
-{
-    // Uso estas variables para almacenar la x e y de la escala
-    float escalaX, escalaY;
 
-    // Update is called once per frame
+{
+    public float velocidadRotacion = 30.0f; // Velocidad a la que inicialmente empezarian a girar
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            escalaX = transform.localScale.x;
-            escalaY = transform.localScale.y;
-            transform.localScale = new Vector2(escalaX - 0.5f
-                , escalaY - 0.5f);
-        }
-        else if (Input.GetKeyDown(KeyCode.G))
-        {
-            escalaX = transform.localScale.x;
-            escalaY = transform.localScale.y;
-            transform.localScale = new Vector2(escalaX + 0.5f
-                , escalaY + 0.5f);
-        }
-        else if (Input.GetKeyDown(KeyCode.I))
-        {
-            // No hace falta usar Time.deltaTime ya que no queremos rotar
-            // de forma constante a lo largo del tiempo
-            transform.Rotate(45 * Vector3.forward);
-        }
-        else if (Input.GetKeyDown(KeyCode.R))
-        {
-            // No hace falta usar Time.deltaTime ya que no queremos rotar
-            // de forma constante a lo largo del tiempo
-            transform.Rotate(45 * Vector3.back);
-        }
-    }
+        // Gira el objeto en el eje z.
+        transform.Rotate 
 }
